95490cfb731b568775acd3075f03230906e41d8c
Dave Austin <57026087+daustin391@users.noreply.github.com>
2021-03-15 11:46:38 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,10 @@
+""" this module contains your credentials for the reddit & fcsapi APIs """
+
+# REDDIT API
+ID = "client_id"
+SECRET = "client_secret"
+USER = "username"
+PASS = "password"
+
+# FCS API
+FCS_KEY = "API Access Key"
